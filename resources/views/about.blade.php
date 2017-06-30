7529c21675daf9d6345db33f2479596fbf6cdc6b
fittwazz <Rasmus@varnamo.it>
2017-06-30 21:24:57 +0200

    Fixed about us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,17 +14,9 @@
         <div class="row">
             <div class="small-16 columns">
                 <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    The heroes awaken team only has one goal, and that is to revive the game EA once made and closed their servers for. Heroes Awakens is a non profit community with open ideas for new suggestions and even new devs or staff members. If you feel like this is any kind of role for you, go to our contact us page and talk to someone there.
                 </p>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                </p>
+
             </div>
         </div>
 
