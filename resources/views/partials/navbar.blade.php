577add346f85c9281d3f1e3ebbbd73acda7acacd
Andreas Poulsen <lyngholmdk@gmail.com>
2017-06-30 16:30:24 +0200

    Added new logo

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,6 @@
 <header id="main-header">
     <div class="row">
+        <div class="small-11 large-6 columns"><a href="{{ route('home') }}"><img src="{{ asset('images/logo_new.png') }}" alt="AwakenHeroes" class="logo"></a></div>
         <nav id="main-nav" class="small-16 large-10 columns">
             <ul class="main-menu">
                 <li><a href="{{ route('home') }}">Home</a></li>
