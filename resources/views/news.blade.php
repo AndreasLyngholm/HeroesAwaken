1510e9522dda14019be18e5626f8a5054cfdca27
fittwazz <Rasmus@varnamo.it>
2017-06-30 21:30:35 +0200

    Added a new news article

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -24,6 +24,43 @@
                     </div>
 
                     <div class="article-content small-16 medium-14 columns">
+                        <h3>Patch notes.</h3>
+                        <p>
+                            I just want to inform you that we have fixed some errors on the sight and fixed som minor issues. Thanks for your support. /Wazz (Web Developer)
+                        </p>
+                    </div>
+                </article>
+
+                <div class="row">
+                    <div class="small-16 columns">
+                        <div class="big-sep"></div>
+                    </div>
+                </div>
+
+            </div>
+
+<section id="main-content" style="margin-top: 50px;">
+
+        <div class="row">
+            <div class="small-16 columns">
+                <h1>News</h1>
+                <div class="big-sep"></div>
+            </div>
+        </div>
+
+        <div class="row blog-entries">
+
+            <!--main-->
+            <div class="small-16 large-16 columns">
+
+                <article class="blog-entry row">
+                    <div class="article-date small-16 medium-2 columns">
+                        <div>
+                            <p>Jun <span class="day">30</span> <span class="year">2017</span></p>
+                        </div>
+                    </div>
+
+                    <div class="article-content small-16 medium-14 columns">
                         <h3>We are bringing back BFH!</h3>
                         <p>
                             The Heroes Awaken project started with a little boys childhood that got ruined when EA closed their servers for Battlefield Heroes 2015-07-14. Makahost saw this oppertunity to prove his love towards people whos childhood was to play Battlefield Heroes. After some trouble with the project, Makahost joined ReviveThe Heroes Awaken project started with a little boys childhood that got ruined when EA closed their servers for Battlefield Heroes 2015-07-14. <br><br> Makahost saw this opportunity to prove his love towards people whos childhood was to play Battlefield Heroes. After some trouble with the project, Makahost joined Revive Network in hope of help with his project. Things did not turn out well and Makahost decided to finish the game on his own and this community is now called Heroes Awaken and is now being developed by a team of geeks.
