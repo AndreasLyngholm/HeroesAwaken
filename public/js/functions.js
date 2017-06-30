b4dd78fdabcf790816622422b1e3c6a6a74e7567
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-01 00:19:26 +0200

    Added more to HTTPS

--- public/js/functions.js
+++ public/js/functions.js
@@ -62,28 +62,3 @@ $(document).ready(function (){
   });
 
 });
-
-/*-----------------------------------------------------------------------------------*/
-/*  CONTACT FORM
-/*-----------------------------------------------------------------------------------*/
-$(document).ready(function (){
-  $("#ajax-contact-form").submit(function() {
-    var str = $(this).serialize();
-
-    $.ajax({
-      type: "POST",
-      url: "includes/contact-process.php",
-      data: str,
-      success: function(msg) {
-          // Message Sent? Show the 'Thank You' message
-          if(msg == 'OK') {
-            result = '<div class="notification_ok">Your message has been sent. Thank you!</div>';
-          } else {
-            result = msg;
-          }
-          $('#note').html(result);
-      }
-    });
-    return false;
-  });
-});
