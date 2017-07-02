e83a848dea024968cfb693ac10521e28057c6923
fittwazz <Rasmus@varnamo.it>
2017-07-02 20:16:26 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,6 @@
     <section id="home-slider">
         <!--slide 1-->
         <div style="background: url({{ asset('images/slider_.png') }})">
-        background-repeat:no-repeat;
-        background-size:cover;
         </div>
         <!--//slide 1-->
         <!--slide-2-->
