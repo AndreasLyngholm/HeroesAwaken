131bda865ab37e5b67bf7813b15585059ca14933
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-02 18:14:09 +0200

    Fixed XSS error

--- app/Http/Controllers/ForumsController.php
+++ app/Http/Controllers/ForumsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Comment;
 use App\Convasation;
 use App\Forum;
+use App\HeroesAwaken\FormValidation;
 use App\Post;
 use App\Topic;
 use Illuminate\Http\Request;
@@ -13,6 +14,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
 
 class ForumsController extends Controller
 {
@@ -28,12 +30,15 @@ class ForumsController extends Controller
         return view('forums.details', compact('forum', 'topics'));
     }
 
-    public function forumsDetailsDoCreate(Forum $forum)
+    public function forumsDetailsDoCreate(Forum $forum, Request $request)
     {
+        $test = str_replace(['&lt;script&gt;'], '', Input::get('text'));
+        $test = str_replace(['&lt;/script&gt;'], '', $test);
+
         Topic::create([
             'name' => Input::get('name'),
             'description' => Input::get('description'),
-            'text' => nl2br(Input::get('text')),
+            'text' => nl2br($test),
             'user_id' => Auth::id(),
             'forum_id' => $forum->id
         ]);
@@ -49,10 +54,13 @@ class ForumsController extends Controller
 
     public function forumsPostsDoCreate(Forum $forum, Topic $topic)
     {
+        $comment = str_replace(['&lt;script&gt;'], '', Input::get('comment'));
+        $comment = str_replace(['&lt;/script&gt;'], '', $comment);
+
         Comment::create([
             'user_id' => Auth::id(),
             'topic_id' => $topic->id,
-            'comment' => nl2br(Input::get('comment'))
+            'comment' => nl2br($comment)
         ]);
         return redirect()->route('forums.posts', [$forum->id, $topic->id]);
     }
