00b3b21d76413e75dc87ce0f5f29e840f02b0f13
Snow <bluesharp97@gmail.com>
2017-07-02 16:32:25 +0200

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <header id="main-header">
     <div class="row">
-        <div class="small-11 large-6 columns"><a href="{{ route('home') }}"><img src="{{ asset('images/logo_small.png') }}" alt="AwakenHeroes" style="width: 50%;" class="logo"></a></div>
+        <div class="small-11 large-6 columns"><a href="{{ route('home') }}"><img src="{{ asset('images/logo_small_.png') }}" alt="AwakenHeroes" style="width: 50%;" class="logo"></a></div>
         <nav id="main-nav" class="small-16 large-10 columns">
             <ul class="main-menu">
                 <li><a @if(Request::is('/')) class="active" @endif href="{{ route('home') }}">Home</a></li>
