d2252a3bb3455c95356e818f66438b9d027a9e34
fittwazz <Rasmus@varnamo.it>
2017-07-03 14:58:25 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
     </section>
 
     <div class="content-top" style="background: url({{ asset('images/backgrounds/bg-body-top.png') }}) repeat-x center top;"></div>
-    <div class="content-top" style="background: url({{ asset('images/downloadbutton.png') }}) repeat-x center top;"></div>
+    
 
     <section id="main-content">
         <div class="row">
