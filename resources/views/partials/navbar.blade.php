aed649e64ea3fd7d2a615454717f7b1b2b2a35dd
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-03 18:30:55 +0200

    Minor test

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<header id="main-header">
+<header id="main-header" @if( ! Request::is('/')) style="background: url({{ asset('images/backgrounds/bg-menu.png') }}) repeat-x center top;" @endif>
     <div class="row">
         <div class="small-11 large-6 columns"><a href="{{ route('home') }}"><img src="{{ asset('images/logo_new_small.png') }}" alt="AwakenHeroes" style="width: 50%;" class="logo"></a></div>
         <nav id="main-nav" class="small-16 large-10 columns">
