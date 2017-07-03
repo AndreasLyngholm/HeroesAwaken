baacc627a70773ae36b5624fb51be367b7bf4ad0
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-03 23:17:24 +0200

    Fixed 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,25 +2,8 @@
 
 @section('content')
 
--    <section class="inner-slider">
- -      <div style="background: url('{{ asset('images/404_slider.png') }}')"></div>
- -    </section>
- 
-    <div class="content-top"></div>
-
-    <section id="main-content">
-
-        <div class="row">
-            <div class="large-16 columns">
-                <h1 style="text-align: center;">404</h1>
-                <h3 style="text-align: center;">The page you tried to visit does not exist!</h3>
-            </div>
-            <div class="clearfix"></div>
-            <div class="large-16 columns">
-                <img class="float-center" src="{{ asset('images/error.png') }}">
-            </div>
-        </div>
-
+    <section class="inner-slider">
+        <div style="background: url({{ asset('images/404_slider.png') }}); height: 750px;"></div>
     </section>
 
 @endsection
