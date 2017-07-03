72c9a09cca436978d60b8f7659efd692eb53f626
fittwazz <Rasmus@varnamo.it>
2017-07-03 22:16:51 +0200

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -124,7 +124,7 @@
                   <!--team-member-->
                     <div class="column team-member">
                         <div class="photo">
-                            <img src="images/about/Wazz1.png" class="" alt="">
+                            <img src="images/about/Wazz.png" class="" alt="">
                         </div>
                         <h5>Wazz</h5>
                         <p>Web developer &amp; Youtuber</p>
