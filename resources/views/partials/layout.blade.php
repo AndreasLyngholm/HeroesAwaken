23227b64cc9648fee5d81949a94e26d4c7b2bdc0
Snow <bluesharp97@gmail.com>
2017-07-04 20:46:00 +0200

    Update layout.blade.php

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HeroesAwaken</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Bitter:700|Open+Sans:400,600,600italic">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v22">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v23">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
     
     <!-- THIS MUST BE LOADED HERE! DO NOT REMOVE -->
