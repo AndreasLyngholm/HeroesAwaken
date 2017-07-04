5c340fc7115ef15034cb799314f28e4eff16e318
Snow <bluesharp97@gmail.com>
2017-07-04 20:37:25 +0200

    Update inner_slider.blade.php

--- resources/views/partials/inner_slider.blade.php
+++ resources/views/partials/inner_slider.blade.php
@@ -1,3 +1,4 @@
 <section class="inner-slider">
+    <img src="{{ asset('images/slider_small.jpg') }}" alt="">
     <div style="background: url('{{ asset('images/slider_small_new.png') }}')"></div>
 </section>
