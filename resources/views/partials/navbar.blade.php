7ccafbf3f384e22877bf8d3dc04432f4369003c4
Rasmus <Rasmus@varnamo.it>
2017-07-05 19:10:29 +0200

    disabled home enabled contact

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,11 +3,11 @@
         <div class="small-11 large-6 columns"><a href="{{ route('home') }}"><img src="{{ asset('images/logo_new_small.png') }}" alt="AwakenHeroes" style="width: 50%;" class="logo"></a></div>
         <nav id="main-nav" class="small-16 large-10 columns">
             <ul class="main-menu">
-                <li><a @if(Request::is('/')) class="active" @endif href="{{ route('home') }}">Home</a></li>
+                {{--<li><a @if(Request::is('/')) class="active" @endif href="{{ route('home') }}">Home</a></li>--}}
                 <li><a @if(Request::is('news')) class="active" @endif href="{{ route('news') }}">News</a></li>
                 <li><a @if(Request::is('forums')) class="active" @endif href="{{ route('forums.lists') }}">Forum</a></li>
                 <li><a @if(Request::is('about')) class="active" @endif href="{{ route('about') }}">About</a></li>
-                {{--<li><a @if(Request::is('contact')) class="active" @endif href="{{ route('contact') }}">Contact</a></li>--}}
+                <li><a @if(Request::is('contact')) class="active" @endif href="{{ route('contact') }}">Contact</a></li>
                 {{--<li><a @if(Request::is('profile')) class="active" @endif href="{{ route('profile.lists') }}">Profile</a></li>--}}
                 @if(Auth::check()) <li><a href="{{ route('doLogout') }}">Logout</a></li>@endif
                 <li>
