1dae78bc76cafcfa45d9e6c3745f4e1c2dc339a2
Snow <bluesharp97@gmail.com>
2017-07-05 21:22:15 +0200

    Update layout.blade.php

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -86,7 +86,7 @@
 </footer>
 
 <script src="{{ asset('js/app-min.js') }}"></script>
-<script src="{{ asset('js/functions.js') }}"></script>
+<script src="{{ asset('js/functions.js') }}?v1"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
 @yield('scripts')
 <script>
