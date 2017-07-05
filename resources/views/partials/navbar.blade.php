8baeaa7e3f33c081dcba6505ac16a6e9bf215fe2
Rasmus <Rasmus@varnamo.it>
2017-07-05 19:27:34 +0200

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="small-11 large-6 columns"><a href="{{ route('home') }}"><img src="{{ asset('images/logo_new_small.png') }}" alt="AwakenHeroes" class="logo"></a></div>
         <nav id="main-nav" class="small-16 large-10 columns">
             <ul class="main-menu">
-                {{--<li><a @if(Request::is('/')) class="active" @endif href="{{ route('home') }}">Home</a></li>--}}
+                <li><a @if(Request::is('/')) class="active" @endif href="{{ route('home') }}">Home</a></li>
                 <li><a @if(Request::is('news')) class="active" @endif href="{{ route('news') }}">News</a></li>
                 <li><a @if(Request::is('forums')) class="active" @endif href="{{ route('forums.lists') }}">Forum</a></li>
                 <li><a @if(Request::is('about')) class="active" @endif href="{{ route('about') }}">About</a></li>
