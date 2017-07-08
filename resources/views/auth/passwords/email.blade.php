49ba85665e2c20259eb3f005141cd72a0299fbff
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-08 18:10:33 +0200

    Add missing file

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,52 @@
-@extends('layouts.app')
+@extends('partials.layout')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+    @include('partials.inner_slider')
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+    <div class="content-top"></div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+    <section id="main-content">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <div class="row">
+            <div class="small-16 columns">
+                <h1>Forgot password?</h1>
+                <div class="big-sep"></div>
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="row" style="margin-bottom: 1rem;">
+            <div class="large-8 columns">
+                <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <label for="email" class="col-md-4 control-label">Email</label>
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="lime-button">
+                                Reset password
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </section>
+
+    <script>
+        @if(Session::has('status'))
+        swal("Success!", "{{ Session::get('status') }}", "success")
+        @endif
+        @if($errors->has('email'))
+            swal("Warning!", "{{ $errors->first('email') }}", "warning")
+        @endif
+    </script>
+
 @endsection
