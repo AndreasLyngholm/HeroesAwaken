982f6be45cb9cc517acddcc562fe187006600811
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-10 22:31:34 +0200

    Made changes to AddFriend

--- resources/views/partials/addFriend.blade.php
+++ resources/views/partials/addFriend.blade.php
@@ -3,6 +3,10 @@
     @if(! Auth::user()->isFriend($user_id))
         @if(\App\FriendRequest::where('receiver', Auth::user()->id)->where('sender', $user_id)->exists())
             <a class="pull-right" href="{{ route('profile.addFriend', $user_id) }}"> <i class="fa fa-user-plus"></i> </a>
+        @elseif(\App\FriendRequest::where('sender', Auth::user()->id)->where('receiver', $user_id)->exists())
+            <i class="fa fa-hourglass-end pull-right"></i>
+        @else
+            <a class="pull-right" href="{{ route('profile.addFriend', $user_id) }}"> <i class="fa fa-user-plus"></i> </a>
         @endif
     @else
         <i class="fa fa-check"></i>
