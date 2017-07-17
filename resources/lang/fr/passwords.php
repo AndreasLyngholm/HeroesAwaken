7024399b86f7da919e44103d9b987a0c369876b0
Warpy <billon.tony71@gmail.com>
2017-07-17 19:32:55 +0200

    Added French Language

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Le mot de passe doit avoir plus de 6 characters.',
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Nous avons envoyé un email de confirmation de réinitialisation du mot de passe!',
+    'token' => 'La session de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne trouvons pas de compte avec cette adresse email.",
+
+];
