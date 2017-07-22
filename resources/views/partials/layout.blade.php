168acb4ae56b393a717be82a8b989932d8162d12
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-22 14:27:59 +0200

    Added new download image

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -37,7 +37,7 @@
 </head>
 <body>
 
-@if( ! Request::is('admin*'))<a href="{{ route('download') }}"><img class="hide-for-small-only" style="position: absolute; left: 1rem; top: 8rem; z-index:2500; width: 10rem;" src="{{ asset('images/dl.png') }}"></a>@endif
+@if( ! Request::is('admin*'))<a href="{{ route('download') }}"><img class="hide-for-small-only" style="position: absolute; left: 1rem; top: 8rem; z-index:2500; width: 10rem;" src="{{ asset('images/dl_new.png') }}"></a>@endif
 
 @if( ! Request::is('admin*'))
     @include('partials.navbar')
