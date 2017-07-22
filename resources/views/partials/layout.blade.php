3e499d9b5c6c7b44bfff1c378fe9a390174cacbc
Max Bosse <max@bosse.io>
2017-07-22 22:46:54 +0200

    don't request stuff from web.archive

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -11,8 +11,8 @@
     @yield('styles')
 
 
-    <script type="text/javascript" src="{{ asset('images/herocreator/js-3.js') }}?v1"></script>
-    <script type="text/javascript" src="{{ asset('images/herocreator/js-2.js') }}?v1"></script>
+    <script type="text/javascript" src="{{ asset('images/herocreator/js-3.js') }}?v2"></script>
+    <script type="text/javascript" src="{{ asset('images/herocreator/js-2.js') }}?v2"></script>
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('images/herocreator/css-3.css') }}?v1" />
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('images/herocreator/css-2.css') }}?v1" />
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/custom.css') }}?v1" />
