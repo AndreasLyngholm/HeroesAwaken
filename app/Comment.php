b86e1ba599241e03b32019aa5f7217a36d7c3563
Andreas Poulsen <lyngholmdk@gmail.com>
2017-07-23 15:51:33 +0200

    Fixed Admin Users Management Details View and Online Users

--- app/Comment.php
+++ app/Comment.php
@@ -14,7 +14,7 @@ class Comment extends Model
 
     public function post()
     {
-        return $this->belongsTo('App\Post');
+        return $this->belongsTo('App\Topic', 'topic_id');
     }
 
     public function author()
--- resources/views/admin/users/management/details.blade.php
+++ resources/views/admin/users/management/details.blade.php
@@ -18,7 +18,7 @@
         <div class="row team">
             <div class="large-16 columns">
                 <div class="large-7 columns">
-                    <div class="panel">
+                    <div>
                         <img src="{{ $user->avatar ? $user->avatar : 'https://via.placeholder.com/250x250' }}" alt="">
                         <h4>User Information</h4>
                         <ul class="no-bullet">
@@ -29,7 +29,7 @@
                             <li>Created at: <strong>{{ $user->created_at }}</strong></li>
                         </ul>
                     </div>
-                    <div class="panel">
+                    <div>
                         <h4>Roles</h4>
                         <ul style="list-style: none;">
                             @foreach($roles as $role => $roleId)
@@ -51,7 +51,7 @@
                 </div>
 
                 <div class="large-9 columns">
-                    <div class="panel callout radius">
+                    <div class="callout radius">
                         <h5>All comments written by the user</h5>
                             <table class="large-16">
                                 <thead>
@@ -65,10 +65,10 @@
                                 <tbody>
                                 @foreach($user->comments as $comment)
                                     <tr>
-                                        <td>{{ \App\Topic::find($comment->topic_id)->name }}</td>
+                                        <td>{{ $comment->post->name }}</td>
                                         <td>{{ $comment->created_at->format('m/d/Y') }}</td>
                                         <td>{!! substr($comment->comment, 0, 50) !!}</td>
-                                        <td><a style="cursor: pointer;" class="label radius success" href="{{ route('forums.posts', [\App\Topic::find($comment->topic_id)->forum_id, $comment->topic_id]) }}#{{ $comment->id }}"><i class="fa fa-info"></i> Details</a></td>
+                                        <td><a style="cursor: pointer;" class="label radius success" href="{{ route('forums.posts', [$comment->post->forum_id, $comment->topic_id]) }}#{{ $comment->id }}"><i class="fa fa-info"></i> Details</a></td>
                                     </tr>
                                 @endforeach
                                 </tbody>
@@ -76,73 +76,6 @@
                     </div>
                 </div>
 
-                {{--<div class="large-9 columns">--}}
-                    {{--<ul class="button-group radius even-4">--}}
-                        {{--<a class="button alert medium"><i class="fa fa-trash"></i> Delete</a>--}}
-                        {{--<a @click="ban" class="button secondary medium"><i class="fa fa-ban"></i> Ban</a--}}
-                    {{--</ul>--}}
-
-                    {{--<div v-show="banning" hidden class="panel">--}}
-                        {{--<h4>Ban User</h4>--}}
-                        {{--<span>Select an option below</span>--}}
-                        {{--<select v-model="banOption">--}}
-                            {{--<option value="ban">Ban</option>--}}
-                            {{--<option value="restrict" selected="">Restrict</option>--}}
-                        {{--</select>--}}
-                        {{--<input type="hidden" name="user" v-model="userId" value="{{ $user->id }}"/>--}}
-                        {{--Expire At:--}}
-                        {{--<input type="date" value="{{ date('Y-m-d', strtotime('+3 months')) }}" name="banEndDate" v-model="banExpire"/>--}}
-                        {{--<div class="small button-group radius even-4">--}}
-                            {{--<a class="button alert small" @click="doBan">Confirm</a>--}}
-                            {{--<a class="button small" @click="closeBan">Cancel</a>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-
-                    {{--<ul class="tabs" data-tab role="tablist">--}}
-                        {{--<li class="tab-title active" role="presentational" ><a href="#panel2-1" role="tab" tabindex="0" aria-selected="true" controls="panel2-1"><i class="fa fa-flag"></i> Events</a></li>--}}
-                        {{--<li class="tab-title" role="presentational" ><a href="#panel2-2" role="tab" tabindex="0"aria-selected="false" controls="panel2-2"><i class="fa fa-money"></i> Payments</a></li>--}}
-                    {{--</ul>--}}
-                    {{--<div class="tabs-content">--}}
-                        {{--<section role="tabpanel" aria-hidden="false" class="content active" id="panel2-1">--}}
-                            {{--<h3><i class="fa fa-flag"></i> Events</h3>--}}
-                            {{--<table style="widows: 100%;" class="large-12">--}}
-                                {{--<thead>--}}
-                                {{--<tr>--}}
-                                    {{--<th>Name</th>--}}
-                                    {{--<th>Username</th>--}}
-                                    {{--<th>Class</th>--}}
-                                    {{--<th>Options</th>--}}
-                                {{--</tr>--}}
-                                {{--</thead>--}}
-                                {{--<tbody>--}}
-                                {{--<tr>--}}
-                                    {{--<th></th>--}}
-                                    {{--<th></th>--}}
-                                    {{--<th></th>--}}
-                                    {{--<th></th>--}}
-                                {{--</tr>--}}
-                                {{--</tbody>--}}
-                            {{--</table>--}}
-                        {{--</section>--}}
-
-                        {{--<section role="tabpanel" aria-hidden="true" class="content" id="panel2-2">--}}
-                            {{--<h3><i class="fa fa-money"></i> Payments</h3>--}}
-                            {{--<table style="widows: 100%;" class="large-12">--}}
-                                {{--<thead>--}}
-                                {{--<tr>--}}
-                                    {{--<th>Payment ID</th>--}}
-                                    {{--<th>Event</th>--}}
-                                    {{--<th>Amount</th>--}}
-                                    {{--<th>Date</th>--}}
-                                {{--</tr>--}}
-                                {{--</thead>--}}
-                                {{--<tbody>--}}
-                                {{--<!-- COMMENTS MADE BY USER -->--}}
-                                {{--</tbody>--}}
-                            {{--</table>--}}
-                        {{--</section>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
             </div>
         </div>
 
@@ -150,17 +83,8 @@
 
 @stop
 
-@section('breadcrumbs')
-<nav class="breadcrumbs" role="menubar" aria-label="breadcrumbs">
-    <li role="menuitem"><a href="#">Home</a></li>
-    <li role="menuitem"><a href="#">Users</a></li>
-    <li role="menuitem"><a href="{{ route('admin.user.manage') }}">Management</a></li>
-    <li role="menuitem" class="current"><a href="#">{{ $user->name }}</a></li>
-</nav>
-@stop
-
 @section('scripts')
-<script type="text/javascript">{{ asset('js/foundation.tooltip.js') }}</script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.15/vue.min.js"></script>
-<script type="text/javascript">{{ asset('js/user.js') }}</script>
+    <script type="text/javascript">{{ asset('js/foundation.tooltip.js') }}</script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.15/vue.min.js"></script>
+    <script type="text/javascript">{{ asset('js/user.js') }}</script>
 @stop
