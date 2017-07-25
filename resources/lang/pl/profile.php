c918321830a044e746a7f317d8fb45613824756b
Snow <bluesharp97@gmail.com>
2017-07-25 18:06:30 +0200

    fix more polish stuff :D

--- resources/lang/pl/profile.php
+++ resources/lang/pl/profile.php
@@ -11,54 +11,54 @@ return [
     */
 
     'friend_list' => 'Lista znajomych',
-    'username' => 'U¿ytkownik',
+    'username' => 'Uzytkownik',
     'status' => 'Status',
     'options' => 'Opcje',
     'remove_friend' => 'Usun znajomego',
-    'pending_request' => 'Masz oczekuj¹ce dodanie do znajomych',
-    'friend_requests' => 'Proœba o dodanie do znajomych',
+    'pending_request' => 'Masz oczekuj&#261;ce dodanie do znajomych',
+    'friend_requests' => 'Prosba o dodanie do znajomych',
 
-    'linked_accounts' => 'Po³¹czone konta',
-    'link_account' => 'Po³¹cz swoje konto discord',
+    'linked_accounts' => 'Po&#322;&#261;czone konta',
+    'link_account' => 'Po&#322;&#261;cz swoje konto discord',
 
     'avatar' => [
         'title' => 'Awatar',
-        'manage' => 'Zmieñ avatar',
-        'below_1' => 'Dodaj swój w³asny awatar.',
-        'below_2' => 'Sugerujemy aby rozmiar obrazu wyniós³ wartoœæ 1:1 ,gdy¿ po udostêpnieniu zostanie on zformatowany.',
+        'manage' => 'zmien avatar',
+        'below_1' => 'Dodaj sw&#243;j w&#322;asny awatar.',
+        'below_2' => 'Sugerujemy aby rozmiar obrazu wyni&#243;s&#322; wartosc 1:1, gdyz po udost&#281;pnieniu zostanie on zformatowany.',
         'submit' => 'Submit avatar',
     ],
 
     'signature' => [
         'title' => 'Sygnatura',
-        'manage' => 'Zmieñ sygnaturê',
-        'below_1' => 'Dodaj w³asn¹ sygnaturê awatar.',
-        'below_2' => 'Sugerujemy aby wysokoœæ i szerokoœc wynosi³a 900x250px',
-        'submit' => 'ZatwierdŸ sygnaturê',
+        'manage' => 'zmien sygnatur&#281;',
+        'below_1' => 'Dodaj w&#322;asn&#261; sygnatur&#281; awatar.',
+        'below_2' => 'Sugerujemy aby wysokosc i szerokosc wynosi&#322;a 900x250px',
+        'submit' => 'zatwierdŸ sygnatur&#281;',
     ],
 
     'description' => [
         'title' => 'Opis',
-        'manage' => 'Zmieñ opis',
+        'manage' => 'zmien opis',
         'submit' => 'Dodaj opis',
     ],
 
-    'change_password' => 'Zmieñ has³o',
-    'current_password' => 'Aktualne has³o',
-    'new_password' => 'Nowe has³o',
-    'new_password_confirmed' => 'Nowe has³o zatwierdzono ',
+    'change_password' => 'zmien has&#322;o',
+    'current_password' => 'Aktualne has&#322;o',
+    'new_password' => 'Nowe has&#322;o',
+    'new_password_confirmed' => 'Nowe has&#322;o zatwierdzono ',
 
-    'change_language' => 'Zmieñ jêzyk',
+    'change_language' => 'zmien j&#281;zyk',
     'en' => 'Angielski',
-    'da' => 'Duñski',
+    'da' => 'Dunski',
     'fr' => 'Francuski',
     'se' => 'Szwedzki',
     'de' => 'Niemiecki',
     'nl' => 'Holenderski',
-    'es' => 'Hiszpañski',
+    'es' => 'Hiszpanski',
     'pl' => 'Polski',
     'ru' => 'Rosyjski',
-    'fi' => 'Fiñski',
+    'fi' => 'Finski',
 
 
 ];
