09577c9253d9034f6f19ca5c49cc2f1f5d6832b7
Spencer Sharkey <spencer@sf-n.com>
2017-07-29 22:41:19 -0400

    add geoip library

--- config/app.php
+++ config/app.php
@@ -170,6 +170,11 @@ return [
         Laravel\Tinker\TinkerServiceProvider::class,
 
         /*
+         * Third-party Service Providers...
+         */
+        \Torann\GeoIP\GeoIPServiceProvider::class,
+
+        /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
@@ -227,6 +232,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'GeoIP' => \Torann\GeoIP\Facades\GeoIP::class
 
     ],
 
