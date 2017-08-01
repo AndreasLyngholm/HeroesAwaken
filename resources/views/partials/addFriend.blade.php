aa6df4b8079d98ac5fab657c40617b3479435953
Snow <bluesharp97@gmail.com>
2017-08-01 18:50:27 +0200

    change "add friend" design again

--- resources/views/partials/addFriend.blade.php
+++ resources/views/partials/addFriend.blade.php
@@ -1,14 +1,7 @@
 @if(Auth::check() && $user_id != Auth::id())
     @if(! Auth::user()->isFriend($user_id))
         @if(\App\FriendRequest::where('receiver', Auth::user()->id)->where('sender', $user_id)->exists())
-            Answer Friend Request:
-            <a style="cursor: pointer;" class="label success" href="{{ route('profile.answerFriendRequest', ['sender' => $user, 'answer' => 'accepted']) }}">
-                <i class="fa fa-check"></i> Accept
-            </a>
-            <a style="cursor: pointer;" class="label alert" href="{{ route('profile.answerFriendRequest', ['sender' => $user, 'answer' => 'declined']) }}">
-                <i class="fa fa-ban"></i> Decline
-            </a>
-
+            <a href="{{ route('profile.addFriend', $user_id) }}">Add Friend <i class="fa fa-user-plus"></i></a>
         @elseif(\App\FriendRequest::where('sender', Auth::user()->id)->where('receiver', $user_id)->exists())
             Friend Request Sent <i class="fa fa-hourglass-end"></i>
         @else
