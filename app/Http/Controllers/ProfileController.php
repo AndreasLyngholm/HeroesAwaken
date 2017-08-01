a6fd688734b4470bdec1fd461e0350418e42cba8
Max Bosse <max@bosse.io>
2017-08-01 06:40:21 +0200

    Try fix change password

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -283,7 +283,11 @@ class ProfileController extends Controller
         if (Hash::check(Input::get('current_password'), Auth::user()->password)) {
             if(Input::get('new_password') == Input::get('new_password_confirmed'))
             {
-                Auth::user()->update(['password', Hash::make(Input::get('new_password'))]);
+                //Auth::user()->update(['password', Hash::make(Input::get('new_password'))]);
+                $user_id = Auth::User()->id; 
+                $obj_user = User::find($user_id);
+                $obj_user->password = Hash::make(Input::get('new_password'));;
+                $obj_user->save();
                 return redirect()->back()->with('success', 'Your password was changed!');
             }
             else
