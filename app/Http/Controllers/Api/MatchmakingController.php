04c64ecf990019a6d6d6e6de83b2d60871ade429
Spencer Sharkey <spencer@sf-n.com>
2017-08-03 01:33:21 -0400

    basic mm logic

--- app/Http/Controllers/Api/MatchmakingController.php
+++ app/Http/Controllers/Api/MatchmakingController.php
@@ -11,7 +11,25 @@ class MatchmakingController extends Controller
         $ip = long2ip($ipint);
         $geo = geoip($ip);
 
+        // I know this isn't the best yet
 
-        echo "1";
+        $games = \DB::select("SELECT a.gid, b.statsValue as `percent_full`
+            FROM games a
+            LEFT JOIN game_server_regions ON a.game_ip = game_server_regions.game_ip
+            LEFt JOIN game_server_stats b ON b.gid = a.gid
+            WHERE a.shard = ?
+                AND (region = ? OR region IS NULL)
+                AND (b.statsKey = 'B-U-percent_full' AND b.statsValue != '100')
+            ORDER BY region DESC, percent_full DESC
+            ", [$shard, $geo['continent']]);
+
+        $results = [];
+
+        foreach ($games as $game)
+        {
+            $results[] = $game->gid;
+        }
+
+        echo join(',', $results);
     }
 }
